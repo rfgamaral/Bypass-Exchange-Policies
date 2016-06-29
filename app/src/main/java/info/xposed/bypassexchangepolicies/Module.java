c549857443b97b41918939a4b079b92a57d586ef
Ricardo Amaral <master@ricardoamaral.net>
2016-06-29 14:28:52 +0100

    Add entry point for all tweaks

---
+++ app/src/main/java/info/xposed/bypassexchangepolicies/Module.java
@@ -0,0 +1,12 @@
+package info.xposed.bypassexchangepolicies;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public final class Module implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    }
+
+}
